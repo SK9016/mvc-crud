720cbf2bf2c20b560d66c8a7c8b3b57f427efc96
Shan Karunarathne <karunarathneshan@gmail.com>
2025-02-19 20:07:06 +0530

    to test the PR template

--- core/Database.php
+++ core/Database.php
@@ -8,7 +8,7 @@ class Database
 
     public function __construct()
     {
-        $config = require __DIR__ . '/../config/database.php'; // Load config
+        $config = require __DIR__ . '/../config/database.php'; // Load configs
         try {
             $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
             $this->pdo = new \PDO($dsn, $config['username'], $config['password']);
